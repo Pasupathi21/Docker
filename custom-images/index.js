d8fbab603898154dd72b67e42dc7683fa3f66074
Pasupathi-M <pasupathi.muthukumar@kprinfo.com>
2023-12-16 21:05:10 +0530

    docker custom images creation

---
+++ custom-images/index.js
@@ -0,0 +1,34 @@
+const express = require('express')
+
+const app = express()
+
+app.get('/', (req, res) => {
+    const { message } = req.query
+    res.send(`
+    <html>
+    <head>
+        <title>Docker image</title>
+        <style>
+            body {
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                padding: 0%;
+                margin: 0%
+            }
+
+        </style>
+    </head>
+    <body>
+        <main>
+            <h3>${message ? message : 'No Message'}</h3>
+        </main>
+    </body>
+</html>
+    `)
+})
+
+app.listen(1215, () => {
+    console.log('app listening on 1215')
+})
+
