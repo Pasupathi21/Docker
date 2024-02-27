7c005e7995083fe3fc9e96f05790bc51b6100df3
Pasupathi-M <pasupathi.muthukumar@kprinfo.com>
2024-02-27 23:19:31 +0530

    new app

---
+++ deploy-to-ec2/chat-fe/src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+     <main>
+      <div >This landing page</div>
+     </main>
+    </>
+  )
+}
+
+export default App
